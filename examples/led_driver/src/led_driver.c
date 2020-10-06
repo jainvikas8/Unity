84c7daaebabc1d48843fc029c06d59d2ac543967
Vikas Katariya <vikas.katariya@arm.com>
2020-10-06 08:31:26 +0100

    Chap 4: Led driver refactor
    Signed-off-by: Vikas Katariya <vikas.katariya@arm.com>

--- examples/led_driver/src/led_driver.c
+++ examples/led_driver/src/led_driver.c
@@ -13,9 +13,14 @@ void led_driver_destroy(void)
 
 }
 
+static uint16_t led_convert_to_bit(uint16_t number)
+{
+    return (1 << (number - 1));
+}
+
 void led_driver_turn_on(uint16_t number)
 {
-    *led_address |= (1 << (number - 1));
+    *led_address |= led_convert_to_bit(number);
 }
 
 void led_driver_turn_off(uint16_t number)
