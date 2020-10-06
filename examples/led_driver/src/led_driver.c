1d822885032df5c14f536e9f27ab92fbf503d844
Vikas Katariya <vikas.katariya@arm.com>
2020-10-06 09:21:27 +0100

    Chap 4: led driver - refactor
    Signed-off-by: Vikas Katariya <vikas.katariya@arm.com>

--- examples/led_driver/src/led_driver.c
+++ examples/led_driver/src/led_driver.c
@@ -8,16 +8,9 @@ enum
     ALL_LEDS_OFF = ~ALL_LEDS_ON
 };
 
-void led_driver_create(uint16_t *address)
+static void update_hardware(void)
 {
-    led_address = address;
-    led_image = ALL_LEDS_OFF;
     *led_address = led_image;
-}
-
-void led_driver_destroy(void)
-{
-
 }
 
 static uint16_t led_convert_to_bit(uint16_t number)
@@ -25,20 +18,32 @@ static uint16_t led_convert_to_bit(uint16_t number)
     return (1 << (number - 1));
 }
 
+void led_driver_create(uint16_t *address)
+{
+    led_address = address;
+    led_image = ALL_LEDS_OFF;
+    update_hardware();
+}
+
+void led_driver_destroy(void)
+{
+
+}
+
 void led_driver_turn_on(uint16_t number)
 {
     led_image |= led_convert_to_bit(number);
-    *led_address = led_image;
+    update_hardware();
 }
 
 void led_driver_turn_off(uint16_t number)
 {
     led_image &= ~led_convert_to_bit(number);
-    *led_address = led_image;
+    update_hardware();
 }
 
 void led_driver_turn_on_all(void)
 {
     led_image = ALL_LEDS_ON;
-    *led_address = led_image;
+    update_hardware();
 }
